893e464ccf5436eabeccd393ffb83292b873abcb
Movicks <codlabx@gmail.com>
2025-03-21 06:14:30 +0100

    feats(language translator context): testing the language translator context

---
+++ app/[locale]/videos/page.tsx
@@ -0,0 +1,9 @@
+// import react from "react"
+
+
+
+export default function Videos(){
+    return(
+        <div>Hello Video</div>
+    )
+}
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Translator from '../Translator'
+
+export default function Navbar() {
+  return (
+    <nav className='bg-white text-black w-full h-[120px] flex items-center justify-between'><div>Navbar</div>    <Translator /></nav>
+  )
+}
