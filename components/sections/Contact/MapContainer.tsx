139be6eaec7bc2adca972016ae6dac89bd0ef0c3
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-03-24 22:04:08 +0000

    feat: update contact page layout and improve map component with custom icon

--- components/sections/Contact/MapContainer.tsx
+++ components/sections/Contact/MapContainer.tsx
@@ -4,24 +4,26 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 
 // Define your location (latitude, longitude)
-const position: [number, number] = [6.5244, 3.3792]; // Example: Lagos, Nigeria
+const position: [number, number] = [51.509097, -0.4737485]; // coordinate according to the originial website
 
 // Custom Leaflet marker icon (to avoid missing marker issue)
 const customIcon = new L.Icon({
-  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  iconSize: [25, 41], // Size of the icon
+  iconUrl: "/svgs/locationIndicator.svg",
+  iconSize: [50, 60], // Size of the icon
   iconAnchor: [12, 41], // Point of the icon which corresponds to marker location
+  className:"animate-pulse scale-125",
+  
 });
 
 const MapComponent = () => {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: "400px", width: "100%", zIndex: "0" }}>
+    <MapContainer  center={position} zoom={13} scrollWheelZoom={false} style={{ height: "400px", width: "100%", zIndex: "0" }}>
       {/* OpenStreetMap Tile Layer */}
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       
       {/* Marker with Popup */}
       <Marker position={position} icon={customIcon}>
-        <Popup>📍 Office Location</Popup>
+        <Popup>📍Estoyonline.es Office Location</Popup>
       </Marker>
     </MapContainer>
   );
--- components/sections/Home/LearnSpanish.tsx
+++ components/sections/Home/LearnSpanish.tsx
@@ -38,27 +38,27 @@ export default function LearnSpanish() {
     },
   })
   return (
-    <section ref={generalSect} className="w-full mt-10 px-4 z-0 relative flex items-center justify-center isolate h-fit">
+    <section ref={generalSect} className="w-full mt-20 px-4 z-0 relative flex items-center justify-center isolate h-fit">
     <span className="absolute pointer-events-none w-fit -left-5 top-5 h-screen block -z-10 overflow-x-hidden">
     <DoubleLeft className="h-full"/>
     </span>
     <span className="absolute pointer-events-none w-fit -right-0 top-10 h-screen block -z-10 overflow-x-hidden">
     <DoubleRight className="h-full relative left-5"/>
     </span>
-    <div className="flex items-center justify-center flex-col gap-3 w-full max-w-3xl">
+    <div className="flex items-center justify-center flex-col gap-5 w-full max-w-3xl">
     <h3 className={
       clsx(
-        "font-bold text-lg sm:text-xl lg:text-2xl duration-500 text-center",
-        show.header?"-translate-y-5 opacity-100":"translate-y-0 opacity-0"
+        "font-bold text-lg sm:text-xl lg:text-2xl duration-500 delay-300 text-center",
+        !show.header?"translate-y-10 opacity-0":"translate-y-0 opacity-100"
       )
     }>Learn Spanish in a fun way at our boutique school.</h3>
     <p className={
       clsx(
-        "text-center duration-500",
-        show.body?"-translate-y-5 opacity-100":"translate-y-0 opacity-0"
+        "text-center duration-500 delay-300",
+        !show.body?"translate-y-10 opacity-0":"translate-y-0 opacity-100"
       )
     }>At our  boutique school Estyonline.es, based in Barcelona, we teach Spanish in fun and engaging way, completely different from traditional methods. Thanks the method techniques we use, learning spanish online has become an enjoyable and exciting process. Fun activities and game increase your motivation and reduce your stress, making language learning easier. Additionally, they encourage natural language use and strengthen social connections.  Learning a language in an enjoyable way enhances retention and effectively helps improve your language skills.Our School has teachers from Spain, Italy, Argentina, and Colombia who are native Spanish speakers. The teacher who conduct beginner-level lessons also speak Turish. All our lessons are conducted online via Zoom </p>
-    <StyledButton >Sample videos from Lesson</StyledButton>
+    <StyledButton className='!mt-2'>Sample videos from Lesson</StyledButton>
     </div>
   </section>
   )
