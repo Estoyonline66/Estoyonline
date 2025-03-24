b7ef419136b5f502d53863bfa72f96292d2dbaed
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-03-24 22:18:56 +0000

    feat: enhance StyledButton component with variant support and update styles

--- components/StyledButton.tsx
+++ components/StyledButton.tsx
@@ -6,13 +6,15 @@ type Props = {
     children: React.ReactNode;
     icon?:React.ReactNode;
     className?:string;
+    variant?:"secondary"|"primary"
 }
 
-export default function StyledButton({children,icon,className}: Props) {
+export default function StyledButton({children,icon,className,variant="secondary"}: Props) {
   return (
     <Button variant={"secondary"} className={
         clsx(
             "!w-fit !text-base !px-6 styledButton",
+            variant,
             className
         )
     }> {icon}{children}</Button>
