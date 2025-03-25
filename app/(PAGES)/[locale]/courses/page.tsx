f0551ed4a6e873c121c08653e50d9831d6541747
Movicks <codlabx@gmail.com>
2025-03-25 23:53:54 +0100

    Translation

--- app/(PAGES)/[locale]/courses/page.tsx
+++ app/(PAGES)/[locale]/courses/page.tsx
@@ -1,18 +1,22 @@
+"use client"
 import Accordion from '@/components/Accordion/Accordion'
 import GeneralHero from '@/components/GeneralHero'
 import Courses from '@/components/majors/Courses'
 import { BookIcon } from '@/components/shapes'
+import { useTranslation } from '@/contexts/TranslationProvider'
+import { CoursesData } from '@/types/PropTypes'
 
 export default function page() {
-    
+    const { t } = useTranslation()
+  const Data: CoursesData = t("courses")
 
   return (
     <>
-    <GeneralHero icon={<BookIcon/>} text='Courses'/>
-    <div className="w-full flex flex-col justify-between gap-2 md:gap-5 pt-20">
-      <Accordion/>  
-      <Courses/>  
-    </div>
+      <GeneralHero icon={<BookIcon />} text={Data.PageTitle} />
+      <div className="w-full flex flex-col justify-between gap-2 md:gap-5 pt-20">
+        <Accordion/>  
+        <Courses/>  
+      </div>
     </>
   )
 }
