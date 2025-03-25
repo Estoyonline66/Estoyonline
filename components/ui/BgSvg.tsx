8341a6b9316f0cabd4354b46f09bcbbe3b5248e3
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-03-25 14:54:35 +0000

    feat: replace animated divs with AnimatedSunLogo component in BgSvg for improved performance

--- components/ui/BgSvg.tsx
+++ components/ui/BgSvg.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import React from 'react'
+import { AnimatedSunLogo } from '../shapes/logo';
 
 export default function BgSvg() {
   return (
@@ -25,44 +26,16 @@ export default function BgSvg() {
           <Image src="/Images/Vector 8.svg" alt="1" width={55} height={200} className='md:w-[6rem] ml-[-1rem]' />
         </motion.div>
 
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{
-            opacity: 1,
-            rotate: [0, -8, 8, 0],
-            y: [0, 12, -12, 0]
-          }}
-          transition={{
-            duration: 3.5,
-            ease: [0.42, 0, 0.58, 1],
-            delay: 0.3,
-            repeat: Infinity,
-            repeatDelay: 5
-          }}
-        >
-          <Image src="/Images/Artboard 1 copy 4ldpi 2.svg" alt="1" width={85} height={100} className='md:w-[15rem] ml-[-1rem]' />
-        </motion.div>
+        <AnimatedSunLogo svg={{
+        className:"size-40 sm:size-60 -translate-x-1/2"
+      }} />
       </div>
 
       {/* Right side */}
       <div className='flex flex-col gap-[2rem] items-end'>
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{
-            opacity: 1,
-            rotate: [0, 5, -5, 0],
-            y: [0, -10, 10, 0]
-          }}
-          transition={{
-            duration: 3.5,
-            ease: [0.42, 0, 0.58, 1],
-            delay: 0.6,
-            repeat: Infinity,
-            repeatDelay: 5
-          }}
-        >
-          <Image src="/Images/Artboard 1.svg" alt="1" width={100} height={100} />
-        </motion.div>
+      <AnimatedSunLogo svg={{
+        className:"size-40 sm:size-60 translate-x-20 sm:translate-x-[calc(var(--spacing)_*_30)]"
+      }} />
 
         <motion.div
           initial={{ opacity: 0, y: 20 }}
