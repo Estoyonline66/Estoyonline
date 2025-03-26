5101ea0b5cbaf1f9fa6701f29268672d699bfca1
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-03-26 15:14:33 +0000

    Refactor JourneyCTA to use video elements and update TestimonialCarousel to include time ago for testimonials

--- components/sections/Home/TestimonialCarousel.tsx
+++ components/sections/Home/TestimonialCarousel.tsx
@@ -20,72 +20,77 @@ export default function TestimonialCarousel() {
     firstComment: string;
     lastComment: string;
     personName: string;
+    timeAgo:string;
   }[] = [
     {
       image: "/Images/testimonialImage (3).png",
-      firstComment:
-        "İspanyolca öğrenmenin bu kadar keyifli olduğunu bilseydim çok daha önce başlardım 🥰 her dersi sabırsızlıkla bekliyorum iyi ki tanıdım sizleri 🌺🌺",
+      firstComment: "İspanyolca öğrenmenin bu kadar keyifli olduğunu bilseydim çok daha önce başlardım 🥰 her dersi sabırsızlıkla bekliyorum iyi ki tanıdım sizleri 🌺🌺",
       lastComment: "If I had known that learning Spanish was this enjoyable",
       personName: "yasemen.avsar",
+      timeAgo: "20 mins"
     },
     {
       image: "/Images/testimonialImage (2).png",
-      firstComment:
-        "Aleja ve onun öğretme yöntemini hep çok sevdim ve bir öğretmenin nasıl olması, nasıl eğlenerek öğretmesi konusunda hep onu örnek veririm 😍",
+      firstComment: "Aleja ve onun öğretme yöntemini hep çok sevdim ve bir öğretmenin nasıl olması, nasıl eğlenerek öğretmesi konusunda hep onu örnek veririm 😍",
       lastComment: "I have always loved Aleja and her teaching method.",
       personName: "umaysirke",
+      timeAgo: "2h"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:
-        "Yıllar önce tanıştığım Aleja ve ekibi sayesinde İspanyolcayı rahatça konuşabilecek ve okuyabilecek şekilde ilerlettim. B1.4 seviyesine kadar sadece bu okuldan ders alarak geldim",
+      firstComment: "Yıllar önce tanıştığım Aleja ve ekibi sayesinde İspanyolcayı rahatça konuşabilecek ve okuyabilecek şekilde ilerlettim. B1.4 seviyesine kadar sadece bu okuldan ders alarak geldim",
       lastComment: "Thanks to Aleja and her team, whom I met years ago",
       personName: "ugurege",
+      timeAgo: "5h"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:
-        "Hola Şarkılarla İspanyolca dersi çok keyifli. Bilmediğim İspanyol şarkıcıları ve ispanyol kültürünü öğreniyorum. Arkadaşlarıma tavsiye edeceğim. İyiki sizi tanımışım.",
+      firstComment: "Hola Şarkılarla İspanyolca dersi çok keyifli. Bilmediğim İspanyol şarkıcıları ve ispanyol kültürünü öğreniyorum. Arkadaşlarıma tavsiye edeceğim. İyiki sizi tanımışım.",
       lastComment: "Şarkılarla yapılan İspanyolca dersleri çok eğlenceli.",
       personName: "muge_dusun",
+      timeAgo: "5min"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:
-        "Harika ogretmenler, harika metodlar. Derslerin nasil gectigini anlamiyorum bile. 2020 den beri sizlerle Ispanyolca ogreniyorum, dersler cok keyifli ve verimli geciyor. Seviyem sizin yardimlarinizla cok iyi seviyeye geldi.",
+      firstComment: "Harika ogretmenler, harika metodlar. Derslerin nasil gectigini anlamiyorum bile. 2020 den beri sizlerle Ispanyolca ogreniyorum, dersler cok keyifli ve verimli geciyor. Seviyem sizin yardimlarinizla cok iyi seviyeye geldi.",
       lastComment: "Harika öğretmenler, harika yöntemler...",
       personName: "meltem_sonmez_canseco",
+      timeAgo: "10min"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:
-        "İspanyolca'ya 3 sene önce sıfırdan başladım. @alejaescuderotuc ve @danielasalzman sayesinde B2.2 seviyesine geldim. İspanyolca öğrenmek isteyenlere şiddetle tavsiye ederim.😍",
+      firstComment: "İspanyolca'ya 3 sene önce sıfırdan başladım. @alejaescuderotuc ve @danielasalzman sayesinde B2.2 seviyesine geldim. İspanyolca öğrenmek isteyenlere şiddetle tavsiye ederim.😍",
       lastComment: "3 yıl önce sıfırdan İspanyolca öğrenmeye başladım.",
       personName: "ulucozuyener",
+      timeAgo: "9h"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:`Gerçekten "wow" dedim öğretmensiniz ve hâlâ kendinizi geliştiriyorsunuz. "Yakında öğrendiğim her şeyi derslerime uygulamaya başlayacağım" ne kadar güzel bir cümle heyecanınızı ben bile hissettim 😊Hoşuma gitti❤️ Hocam❤️`,
+      firstComment: `Gerçekten "wow" dedim öğretmensiniz ve hâlâ kendinizi geliştiriyorsunuz. "Yakında öğrendiğim her şeyi derslerime uygulamaya başlayacağım" ne kadar güzel bir cümle heyecanınızı ben bile hissettim 😊Hoşuma gitti❤️ Hocam❤️`,
       lastComment: `Gerçekten "Vay be!" dedim. Siz bir öğretmensiniz ve hala kendinizi geliştiriyorsunuz.`,
       personName: "17limerence",
+      timeAgo: "10h"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:`Daha 1 seviye geçip diğerine geçtim. Carolina ile derslerimiz çok eğleneceli geçiyor, çok verimli, bilgilerle dolu dolu, hocamız çok güler yüzlü ve çok yardımcıdır. İyi ki yolumuz kesişti @estoyonline.es sizi seviyorum, sizinle konuşma korkusunu aştım ve çok motive oldum dili öğrenmeye devam etmek için. Herkese şiddetle kursunuzu tavsiye ederim!😍`,
+      firstComment: `Daha 1 seviye geçip diğerine geçtim. Carolina ile derslerimiz çok eğleneceli geçiyor, çok verimli, bilgilerle dolu dolu, hocamız çok güler yüzlü ve çok yardımcıdır. İyi ki yolumuz kesişti @estoyonline.es sizi seviyorum, sizinle konuşma korkusunu aştım ve çok motive oldum dili öğrenmeye devam etmek için. Herkese şiddetle kursunuzu tavsiye ederim!😍`,
       lastComment: `Pandemiden beri harika bir yolculuk oldu.`,
       personName: "allaunal_embroidery",
+      timeAgo: "1h"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:`Recomiendo las lecciones de Aleja, son muy divertido`,
+      firstComment: `Recomiendo las lecciones de Aleja, son muy divertido`,
       lastComment: `Pandemiden beri harika bir yolculuk oldu.`,
       personName: "ebru_basaran35",
+      timeAgo: "7mins"
     },
     {
       image: "/Images/testimonialImage (1).png",
-      firstComment:`Aleja ve ekibine Teşekkürler. İspanyolca öğrenirken en iyi yani ogretmenlerin İspanyol ve Türkçe biliyor olmaları..Anlamamız kolay oluyor..bir yabancı dil öğretmeni olarak üçüncü bir dil İspanyolca ya iki ay önce başladım.. A1.1 deyim zamanım oldukça öğrenmeye devam edeceğim.. teşekkürler @clasesconaleja ve Melis yeni kurda görüşmek üzere`,
+      firstComment: `Aleja ve ekibine Teşekkürler. İspanyolca öğrenirken en iyi yani ogretmenlerin İspanyol ve Türkçe biliyor olmaları..Anlamamız kolay oluyor..bir yabancı dil öğretmeni olarak üçüncü bir dil İspanyolca ya iki ay önce başladım.. A1.1 deyim zamanım oldukça öğrenmeye devam edeceğim.. teşekkürler @clasesconaleja ve Melis yeni kurda görüşmek üzere`,
       lastComment: `Aleja ve ekibine teşekkürler.`,
       personName: "yollarinrengL",
+      timeAgo: "just now"
     }
   ];
 
@@ -96,7 +101,7 @@ export default function TestimonialCarousel() {
         {Data.testimonies ? Data.testimonies : defaultData.testimonies}
       </h3>
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-        <Marquee pauseOnHover className="[--duration:20s]">
+        <Marquee pauseOnHover className="[--duration:50s]">
           {testimonials.map((testimony, index) => (
             <div
               key={index}
@@ -116,7 +121,7 @@ export default function TestimonialCarousel() {
                     <strong className="text-sm shrink max-w-full min-[360px]:max-w-[40%] break-words">{testimony.personName}</strong>
                     <span className="w-full justify-between min-[360px]:justify-start min-[360px]:w-fit flex items-center gap-2 shrink-0">
                       <em className="text-xs not-italic text-neutral-400">
-                        20 mins
+                        {testimony.timeAgo}
                       </em>
                       <div className="flex items-center gap-2">
                       <HeartIcon
