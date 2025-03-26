eee47f294777c3a70853741c21c13e4f350e5caa
Asterixh <92242499+CodeWithAsterixh@users.noreply.github.com>
2025-03-26 09:44:34 +0000

    Refactor Teachers page to remove duplicate TeacherCard mappings; adjust brightness in Hero section background; update logo animation timing and scaling

--- components/sections/Home/Hero.tsx
+++ components/sections/Home/Hero.tsx
@@ -4,7 +4,7 @@ import HeroContent from "./HeroContent";
 export default function HomeHero() {
   return (
     <section className="w-full min-h-screen py-40 px-4 md:px-10 lg:px-20 z-0 relative isolate flex items-center">
-      <span className="block size-full -z-10 absolute inset-0 brightness-[.3] isolate">
+      <span className="block size-full -z-10 absolute inset-0 brightness-[.7] isolate">
         <ImageBox src="/Images/homeImage.png" width={1440} height={1080} className="size-full absolute inset-0 object-center object-cover -z-10"/>
         <video
           src="/videos/banner.mp4"
