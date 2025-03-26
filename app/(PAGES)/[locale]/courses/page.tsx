fcd22b2f353b6a252d11ac4ffdf6f6423d36e2aa
Movicks <codlabx@gmail.com>
2025-03-26 07:57:17 +0100

    SEO TAGS

--- app/(PAGES)/[locale]/courses/page.tsx
+++ app/(PAGES)/[locale]/courses/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Accordion from "@/components/Accordion/Accordion";
 import GeneralHero from "@/components/GeneralHero";
-import Courses from "@/components/majors/Courses";
+import Courses from "@/components/sections/Courses/Courses";
 import Meta from "@/components/Meta";
 import { BookIcon } from "@/components/shapes";
 import { useTranslation } from "@/contexts/TranslationProvider";
