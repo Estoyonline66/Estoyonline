f0651a52e5970f359ee690a884efaa76f7939599
erdemtuc <erdemtuc@gmail.com>
2025-04-08 23:06:05 +0200

    no message

--- components/sections/Contact/MapContainer.tsx
+++ components/sections/Contact/MapContainer.tsx
@@ -9,7 +9,8 @@ const position: [number, number] = [41.384867441618674, 2.16944012016164]; // co
 // Custom Leaflet marker icon (to avoid missing marker issue)
 const customIcon = new L.Icon({
   iconUrl: "/svgs/locationIndicator.svg",
-  iconSize: [50, 60], // Size of the icon
+ // iconSize: [50, 60], // Size of the icon
+  iconSize: [0, 0], // Size of the icon
   iconAnchor: [12, 41], // Point of the icon which corresponds to marker location
   className:"animate-pulse scale-125",
   
@@ -21,13 +22,11 @@ const MapComponent = () => {
       {/* OpenStreetMap Tile Layer */}
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
   
-      {/* Marker with Popup */
-	  /*
+      {/* Marker with Popup */}
       <Marker position={position} icon={customIcon}>
         <Popup>📍Estoyonline.es Office Location</Popup>
       </Marker>
-	  */
-		}
+
     </MapContainer>
   );
 };
