961580efdb3de78d89b59db4dfd4cf847885ce62
erdemtuc <erdemtuc@gmail.com>
2025-04-24 01:26:52 +0200

    no message

--- components/sections/Home/HeroContent.tsx
+++ components/sections/Home/HeroContent.tsx
@@ -11,11 +11,11 @@ export default function HeroContent() {
   const { t } = useTranslation();
   const Data: HomeProps = t("home");
   const defaultData = {
-   "HeroTitle": "",
-    "HeroYellowTitle": "",
-    "HeroTitle2": "",
+   "HeroTitle": "Dünya",
+    "HeroYellowTitle": "İspanyolca konuşuyor,",
+    "HeroTitle2": "ya sen?",
      "HeroDescription": "",
-    "HeroButton": "",
+    "HeroButton": "Kursları incele",
   };
   return (
     <div className="w-fit p-1 flex flex-col gap-5 animate-[pulse_1s_linear_forwards] text-white relative max-w-[95vw] min-[498px]:max-w-sm sm:max-w-lg lg:max-w-xl">
