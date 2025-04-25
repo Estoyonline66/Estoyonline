3f845a85a5775dd88ef89ed6a043bf56d7f762f4
erdemtuc <erdemtuc@gmail.com>
2025-04-25 02:53:13 +0200

    no message

--- components/MenuIcon.tsx
+++ components/MenuIcon.tsx
@@ -7,17 +7,16 @@ export default function MenuIcon(props: {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="22"
-      viewBox="0 0 32 22"
+      width="40"
+      height="30"
+      viewBox="0 0 40 30"
       fill="none"
       {...props.svg}
     >
       <path
-        d="M1 11H31M1 1H31M11 21H31"
+        d="M2 15H38M2 5H38M12 25H38"
         stroke="#FEFEFE"
-        strokeOpacity="0.996078"
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
         {...props.path}
