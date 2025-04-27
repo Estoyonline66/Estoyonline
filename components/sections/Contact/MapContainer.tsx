28b6782c1478225f6b072e250b020de2cbba8306
erdemtuc <erdemtuc@gmail.com>
2025-04-28 00:50:33 +0200

    no message

--- components/sections/Contact/MapContainer.tsx
+++ components/sections/Contact/MapContainer.tsx
@@ -1,32 +1,43 @@
-"use client"; 
+"use client";
 
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 
 // Define your location (latitude, longitude)
-const position: [number, number] = [41.384867441618674, 2.16944012016164]; // coordinate according to the originial website
+const position: [number, number] = [41.384867441618674, 2.16944012016164];
 
-// Custom Leaflet marker icon (to avoid missing marker issue)
+// Custom Leaflet marker icon
 const customIcon = new L.Icon({
   iconUrl: "/svgs/locationIndicator.svg",
- // iconSize: [50, 60], // Size of the icon
-  iconSize: [0, 0], // Size of the icon
-  iconAnchor: [12, 41], // Point of the icon which corresponds to marker location
-  className:"animate-pulse scale-125",
-  
+  iconSize: [0, 0],
+  iconAnchor: [12, 41],
+  className: "animate-pulse scale-125",
 });
 
 const MapComponent = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    // Sunucuda render aşamasında hiçbir şey gösterme
+    return null;
+  }
+
   return (
-    <MapContainer  center={position} zoom={13} scrollWheelZoom={false} style={{ height: "400px", width: "100%", zIndex: "0" }}>
-      {/* OpenStreetMap Tile Layer */}
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "400px", width: "100%", zIndex: "0" }}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-  
-      {/* Marker with Popup */}
       <Marker position={position} icon={customIcon}>
         <Popup>📍Estoyonline.es Office Location</Popup>
       </Marker>
-
     </MapContainer>
   );
 };
