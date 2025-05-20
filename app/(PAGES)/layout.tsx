6d1453cd67343405149c1c4cee554887459d7db6
erdemtuc <erdemtuc@gmail.com>
2025-05-20 22:44:52 +0200

    no message

--- app/(PAGES)/layout.tsx
+++ app/(PAGES)/layout.tsx
@@ -10,6 +10,8 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <>
+	<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="b6efb9d0-82ac-41af-b406-a6bba12d150c" type="text/javascript" async></script>
+	
       {/* Google Ads Tag (gtag.js) */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-1040846615"
