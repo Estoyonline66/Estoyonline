3cfa3b537f9e2e757ebcfa607bf58141f4c8ad96
erdemtuc <erdemtuc@gmail.com>
2025-07-21 12:51:56 +0200

    no message

--- app/api/save-form-data/route.js
+++ app/api/save-form-data/route.js
@@ -35,7 +35,7 @@ async function backupToFTP() {
       host: process.env.FTP_HOST,
       user: process.env.FTP_USER,
       password: process.env.FTP_PASSWORD,
-      secure: process.env.FTP_SECURE === 'true',
+      secure: process.env.FTP_SECURE === 'false',
       port: process.env.FTP_PORT || 21
     });
 
