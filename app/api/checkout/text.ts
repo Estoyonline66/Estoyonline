1c08a4630ba7abedbb9d7574ca9842f67511e595
erdemtuc <erdemtuc@gmail.com>
2025-09-09 12:25:10 +0200

    no message

--- app/api/checkout/text.ts
+++ app/api/checkout/text.ts
@@ -16,7 +16,7 @@ export const courseMap: Record<string, CourseInfo> = {
   "Complementary_course_v7Qe": { name: "Complementary Course", amount: 12000, currency: "eur" },
   "Upper_level_courses_R82e": { name: "Upper level courses", amount: 19000, currency: "eur" },
   "Examen_Rt6B": { name: "Examen", amount: 1500, currency: "eur" },
-   "Test_Rt6B": { name: "Test", amount: 30, currency: "try" },
+   "Test_Rt6B": { name: "Test", amount: 5000, currency: "try" },
 
   // Özel dersler – yetişkin
   "10_derslik_yetişkin_özel_ders_paketi_Az1R": { name: "Özel Ders (1 yetişkin, 10 ders)", amount: 22000, currency: "eur" },
