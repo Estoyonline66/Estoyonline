fd54bd64e101090bb1d69b68b351b0e2c91c8b84
erdemtuc <erdemtuc@gmail.com>
2025-12-15 15:43:33 +0100

    no message

--- app/api/checkout/text.ts
+++ app/api/checkout/text.ts
@@ -6,17 +6,19 @@ export interface CourseInfo {
 
 export const courseMap: Record<string, CourseInfo> = {
   // Grupales
-  "A1.1_başlangıç_kursu_Türkiye_ab1X": { name: "A1.1 Başlangıç (Türkiye)", amount: 798000, currency: "try" },
+  "A1.1_başlangıç_kursu_Türkiye_ab1X": { name: "A1.1 Başlangıç (Türkiye)", amount: 670000, currency: "try" },
   "A1.1_başlangıç_kursu_Yurtdışı_q9Z2": { name: "A1.1 Başlangıç (Yurtdışı)", amount: 19000, currency: "eur" },
-  "A1.1_sabah_Türkiye_Xy7p": { name: "A1.1 Sabah (Türkiye)", amount: 660000, currency: "try" },
-  "Üst_seviyeler_Türkiye_T6mQ": { name: "Üst Seviyeler (Türkiye)", amount: 612000, currency: "try" },
+  "A1.1_sabah_Türkiye_Xy7p": { name: "A1.1 Sabah (Türkiye)", amount: 550000, currency: "try" },
+  "Üst_seviyeler_Türkiye_T6mQ": { name: "Üst Seviyeler (Türkiye)", amount: 510000, currency: "try" },
+  "Üst_seviyeler_indirimli_Türkiye_D7k2": { name: "Üst Seviyeler İndirimli (Türkiye)", amount: 460000, currency: "try" },
   "Üst_seviyeler_Yurtdışı_J8d4": { name: "Üst Seviyeler (Yurtdışı)", amount: 14500, currency: "eur" },
-  "Tamamlayıcı_kurs_Türkiye_w2Np": { name: "Tamamlayıcı Kurs (Türkiye)", amount: 318000, currency: "try" },
+  "Tamamlayıcı_kurs_Türkiye_w2Np": { name: "Tamamlayıcı Kurs (Türkiye)", amount: 265000, currency: "try" },
   "Tamamlayıcı_kurs_Yurtdışı_Lp5k": { name: "Tamamlayıcı Kurs (Yurtdışı)", amount: 9000, currency: "eur" },
   "Complementary_course_ext_v7Qe": { name: "Complementary Course", amount: 12000, currency: "eur" },
   "Upper_level_courses_ext_R82e": { name: "Upper level courses", amount: 19000, currency: "eur" },
   "Examen_Rt6B": { name: "Examen", amount: 1500, currency: "eur" },
- 
+   "Test_Rt6B": { name: "Test", amount: 5000, currency: "try" },
+
 
   "10_derslik_özel_ders_paketi_Az1R": { name: "Özel Ders (1 kişi, 10 ders)", amount: 22000, currency: "eur" },
   "5_derslik_özel_ders_paketi_Nm8t": { name: "Özel Ders (1 kişi, 5 ders)", amount: 11000, currency: "eur" },
@@ -29,5 +31,5 @@ export const courseMap: Record<string, CourseInfo> = {
   "10_derslik_5_kişilik_grup_Lm3N": { name: "Özel Ders (5 kişi, 10 ders)", amount: 45000, currency: "eur" },
   "5_derslik_5_kişilik_özel_ders_paketi_Yr2P": { name: "Özel Ders (5 kişi, 5 ders)", amount: 22500, currency: "eur" },
 
- 
+
 };
